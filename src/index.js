caa09c46c4ef52cf14db77c2877e2f75c600a737
trinhhoaibaotkqn <trinhhoaibaotkqn@gmail.com>
2022-09-18 17:52:57 +0300

    Install Nodemon, Morgan, Template engine(hbs),scss

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require("express-handlebars");
+
+const app = express();
+const port = 3000;
+
+//use static file and css in path src/public 
+app.use(express.static(path.join(__dirname,'public')))
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine (npm install express-handlebars)
+const handlebars = exphbs.create({ extname: '.hbs',});//.handlebars->.hbs
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+app.set('views',path.join(__dirname,'resources/views'))
+
+// app.engine('handlebars', handlebars())
+// app.set('view engine','handlebars');
+
+
+app.get('/', (req, res) => {
+  // res.send('alo');
+  res.render("home");//body
+})
+
+app.get('/news', (req, res) => {
+  res.render("news");//body
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening http://localhost:${port}`)
+})
