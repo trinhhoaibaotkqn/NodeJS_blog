cf81768ddf88a71d1fe4db1499f277ef26a6a0af
trinhhoaibaotkqn <trinhhoaibaotkqn@gmail.com>
2022-09-19 20:02:25 +0300

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
-const route = require('./routes')//tự động chọc vào file index
+const route = require('./routes'); //tự động chọc vào file index
 
-//use static file and css in path src/public 
-app.use(express.static(path.join(__dirname,'public')))
+//use static file and css in path src/public
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Middleware
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 app.use(express.json());
 
 //HTTP logger
 //app.use(morgan('combined'));
 
 //Template engine (npm install express-handlebars)
-const handlebars = exphbs.create({ extname: '.hbs',});//.handlebars->.hbs
-app.engine('.hbs', handlebars.engine);
-app.set('view engine', '.hbs');
-app.set('views',path.join(__dirname,'resources/views'))
-
+const handlebars = exphbs.create({ extname: '.hbs' }); //.handlebars->.hbs
+      app.engine('.hbs', handlebars.engine);
+      app.set('view engine', '.hbs');
+      app.set('views', path.join(__dirname, 'resources/views'));
 
 //Route init
 route(app);
@@ -37,5 +38,5 @@ route(app);
 
 //Start web server nhờ express
 app.listen(port, () => {
-  console.log(`Example app listening http://localhost:${port}`)
-})
+  console.log(`Example app listening http://localhost:${port}`);
+});
