b33b12bc54ac81ba119889e10fb4edd0fd37de3a
trinhhoaibaotkqn <trinhhoaibaotkqn@gmail.com>
2022-09-21 00:32:51 +0300

    Fix erorr merge

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+//Connect to MongoDB
 async function connect(){
     try {
+        //await bất đồng bộ nên thêm async 
         await mongoose.connect('mongodb://localhost:27017/f8_education_dev');
         console.log('Connect successfully');
     } catch (error) {
