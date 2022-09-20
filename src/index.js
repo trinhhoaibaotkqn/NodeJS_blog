304357e91592d374043a75c1f1f90c5e1b38a333
trinhhoaibaotkqn <trinhhoaibaotkqn@gmail.com>
2022-09-21 00:13:08 +0300

    Create Model, connect to DB, Controller <-> Model

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
-const route = require('./routes')//tự động chọc vào file index
+const route = require('./routes'); //tự động chọc vào file index
+const db = require('./config/db');
 
-//use static file and css in path src/public 
-app.use(express.static(path.join(__dirname,'public')))
+//Connect to DB
+db.connect();
+//use static file and css in path src/public
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Middleware
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 //HTTP logger
 //app.use(morgan('combined'));
 
 //Template engine (npm install express-handlebars)
-const handlebars = exphbs.create({ extname: '.hbs',});//.handlebars->.hbs
+const handlebars = exphbs.create({ extname: '.hbs' }); //.handlebars->.hbs
 app.engine('.hbs', handlebars.engine);
 app.set('view engine', '.hbs');
-app.set('views',path.join(__dirname,'resources/views'))
-
+app.set('views', path.join(__dirname, 'resources/views'));
 
 //Route init
 route(app);
@@ -37,5 +41,5 @@ route(app);
 
 //Start web server nhờ express
 app.listen(port, () => {
-  console.log(`Example app listening http://localhost:${port}`)
-})
+    console.log(`App listening at http://localhost:${port}`);
+});
