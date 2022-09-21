46e811e0325f034b73c49a3d4e7f600c81701b04
trinhhoaibaotkqn <trinhhoaibaotkqn@gmail.com>
2022-09-22 00:18:04 +0300

    Read from DB and render in view

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,26 @@
 const Course = require('../models/Course')
+const { mutipleMongooseToObject } = require('../../util/mongoose')
+
 class SiteController{
     // [GET] /home
-    home(req,res){
-        Course.find({},function(err,courses){
-            if(!err){
-                res.json(courses);
-            }else{
-                res.status(400).json({ error: 'ERROR!!!'})
-            }
-        });
+    home(req,res,next){//next - middleware
+        //Use callback
+        // Course.find({},function(err,courses){
+        //     if(!err){
+        //         res.json(courses);
+        //     }else{
+        //         next(err);
+        //         // res.status(400).json({ error: 'ERROR!!!'})
+        //     }
+        // });
+        //Use promise
+        Course.find({})
+            .then(courses => {
+                courses = mutipleMongooseToObject(courses);
+                //handlebars đưa về object nằm trong proto cần toObject trong until
+                res.render('home',{ courses })//courses: courses
+            })
+            .catch(next)//.catch(err => next(err))
         // res.render('home');
     }
 
--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes'); //tự động chọc vào file index
-const db = require('./config/db');
 
+const db = require('./config/db');
 //Connect to DB
 db.connect();
 
@@ -19,7 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
   extended: true
 }))
-
 app.use(express.json());
 
 //HTTP logger
