b92df904038223c07530fe2ac1061ed04608a9c7
trinhhoaibaotkqn <trinhhoaibaotkqn@gmail.com>
2022-09-28 16:26:23 +0300

    Delete course [method DELETE] html modal

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -7,6 +7,7 @@ router.get('/create', coursesController.create);
 router.post('/store', coursesController.store);
 router.get('/:id/edit', coursesController.edit);
 router.put('/:id', coursesController.update);
+router.delete('/:id', coursesController.delete);
 router.get('/:slug', coursesController.show);
 
 module.exports = router;
